256858316819570211f1260c5764438d81e78c1a
Justinas Globys <justinas.globys@gmail.com>
2024-10-24 20:23:27 +0300

    Add escape button handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Modal from "./components/Modal";
 import Card from "./components/Card";
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import { useOutsideClick } from "./hooks/useOutsideClick";
 
 function App() {
@@ -13,13 +13,34 @@ function App() {
     setIsModalOpen(true);
   };
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     setIsModalOpen(false);
-  };
+  }, [setIsModalOpen]);
 
   // Close Modal after clicking outside of it
   useOutsideClick(modalRef, closeModal);
 
+  // Closes modal after detecting escape key event
+  const handleEscButton = useCallback(
+    (event) => {
+      if (event.key !== "Escape") return;
+
+      closeModal();
+    },
+    [closeModal]
+  );
+
+  // Close modal when "esc" is pressed
+  useEffect(() => {
+    if (!isModalOpen) return;
+
+    document.addEventListener("keydown", handleEscButton, false);
+    return () => {
+      document.removeEventListener("keydown", handleEscButton, false);
+    };
+  }, [isModalOpen, handleEscButton]);
+
+  // Reuse the same title and content for card and modal
   const title = "Lorem ipsum";
   const content =
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.";
